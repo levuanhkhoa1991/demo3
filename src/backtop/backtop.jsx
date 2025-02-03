0a997270a493e13c9896497b6d0762714c8484f1
unknown <hamdoi317@gmail.com>
2025-02-04 00:50:45 +0700

    update

--- src/backtop/backtop.jsx
+++ src/backtop/backtop.jsx
@@ -1,7 +1,5 @@
-
-
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from "react";
+import "../backtop/backtop.css";
 
 const BackToTop = () => {
   const [showButton, setShowButton] = useState(false);
@@ -15,43 +13,44 @@ const BackToTop = () => {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   };
 
   return (
     showButton && (
-      <button onClick={scrollToTop} style={styles.backToTopButton}>
-        Top
-      </button>
+      <button
+        onClick={scrollToTop}
+        className="backToTopButton"
+        style={styles.backToTopButton}
+      ></button>
     )
   );
 };
 
 const styles = {
   backToTopButton: {
-    position: 'fixed',
-    bottom: '1vw',
-    right: '1vw',
-    padding: '1rem',
-    fontSize: '16px',
-    backgroundColor: '#007bff',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '5%',
-    cursor: 'pointer',
+    position: "fixed",
+    bottom: "1vw",
+    right: "1vw",
+    padding: "2vw",
+    fontSize: "16px",
+    backgroundColor: "#007bff",
+    color: "#fff",
+    border: "none",
+    borderRadius: "50%",
+    cursor: "pointer",
     zIndex: 1000,
   },
-  
 };
 
 export default BackToTop;
